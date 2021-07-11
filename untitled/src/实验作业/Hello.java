d2cb0cd0647a9737703311430c3585eda1e64b39
wang-zhe-pro <2426654824@qq.com>
2021-07-11 14:48:37 +0800

    最新版本

--- untitled/src/实验作业/Hello.java
+++
@@ -1,7 +0,0 @@
-package 实验作业;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-    }
-}
--- untitled/src/异常处理综合应用/ShapeException.java
+++
@@ -1,9 +0,0 @@
-package 异常处理综合应用;
-
-public class ShapeException extends Exception {
-    ShapeException(){};
-    ShapeException(String s){
-        super(s);
-    }
-
-}
